ad1b5833f5681dc6571e924efd4d77c0f734645c
Narcoker <jykim9868@gmail.com>
2022-01-22 01:05:42 +0900

    bugfix

--- src/AccessPage.js
+++ src/AccessPage.js
@@ -12,20 +12,18 @@ function AccessPage() {
 
   const [stdID, setStdID] = useState("");
   const [name, setName] = useState("");
-  const [major, setMajor] = useState("");
+  // const [major, setMajor] = useState(""); 없어도될듯?
   const [password, setPassword] = useState("");
   const [checkPassword, setCheckPassword] = useState("");
   const [email, setEmail] = useState("");
   const [certFile, setCertFile] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
-  // let [majorList, setMajorList] = useState(["학과정보를 불러올 수 없습니다."]);
   const [majorList, setMajorList] = useState(["국어국문학과", "영어영문학부", "일어일문학부", "사학과", "경제학부", "법학과", "행정학과", "국제지역학부", "중국학과", "신문방송학과", "정치외교학과", "유아교육과", "시각디자인학과", "공업디자인학과", "패션디자인학과", "경영학부", "국제통상학부", "응용수학과", "통계학과", "물리학과", "화학과", "미생물학과", "해양스포츠학과", "간호학과", "과학시스템시뮬레이션학과", "건축공학과", "건축학과", "소방공학과", "시스템경영공학부", "IT융합응용공학과", "안전공학과", "융합디스플레이공학과", "의공학과", "전기공학과", "전자공학과", "정보통신공학과", "제어계측공학과", "조선해양시스템공학과", "컴퓨터공학과", "토목공학과", "고분자공학과", "공업화학과", "금속공학과", "기계공학과", "기계설계공학과", "기계시스템공학과", "냉동공조공학과", "신소재시스템공학과", "인쇄정보공학과", "재료공학과", "화학공학과", "지속가능공학부", "식품공학과", "해양바이오신소재학과", "해양생산시스템관리학부", "해양수산경영학과", "수해양산업교육과", "자원생물학과", "식품영양학과", "생물공학과", "수산생명의학과", "환경공학과", "해양공학과", "해양학과", "지구환경과학과", "환경대기과학과", "에너지자원공학과", "공간정보시스템공학과", "생태공학과", "데이터정보과학부(빅데이터융합전공)", "데이터정보과학부(통계·데이터사이언스전공)", "미디어커뮤니케이션학부(언론정보전공)", "미디어커뮤니케이션학부(휴먼ICT융합전공)", "스마트헬스케어학부(의공학전공)", "스마트헬스케어학부(해양스포츠전공)", "스마트헬스케어학부(휴먼바이오융합전공)", "전자정보통신공학부(전자공학전공)", "전자정보통신공학부(정보통신공학전공)", "조형학부(건축학전공)", "조형학부(공업디자인전공)", "조형학부(시각디자인전공)", "컴퓨터공학부(소프트웨어·인공지능전공)", "컴퓨터공학부(컴퓨터공학전공)", "평생교육·상담학과", "기계조선융합공학과", "전기전자소프트웨어공학과", "공공안전경찰학과"]);
-
   const [isCorrect, setIsCorrect] = useState([false, false, false, false, false, false, false, false])
   const [emailTypeState, setEmailTypeState] = useState(false);
-
   const [resendEmail, setResendEmail] = useState(0);
   const [signUpButtonState, setSignUpButtonState] = useState(false);
+
   const history = useHistory();
 
 
@@ -66,7 +64,8 @@ function AccessPage() {
     if (position === "president") {
       if (isCorrect.includes(false))
         setSignUpButtonState(false);
-    } else setSignUpButtonState(true);
+      else setSignUpButtonState(true);
+    }
 
     if (position === "student") {
       for (let i = 0; i < 8; i++) {
@@ -84,13 +83,19 @@ function AccessPage() {
   function reset() {
     setStdID("");
     setName("");
-    setMajor("");
+    // setMajor(""); 없어도될듯?
+
     setPassword("");
     setCheckPassword("");
     setEmail("");
     setCertFile("");
     setIsCorrect([false, false, false, false, false, false, false, false]);
     setResendEmail(0);
+
+    if (document.getElementById("major") !== null) {
+      document.getElementById("major").value = "";
+    }
+
 
   };
 
@@ -108,7 +113,7 @@ function AccessPage() {
         .then((payload) => {
           console.log(payload);
           if (payload.data.position === "student") {
-            history.push('/main/' + payload.data.sclData.studentPresident.major); // 수정 필요
+            history.push('/main/' + payload.data.sclData.studentPresident.major);
           }
           else if (payload.data.position === "president") {
             history.push('/manage/' + payload.data.sclData.studentPresident.major); // 수정 필요
@@ -170,11 +175,6 @@ function AccessPage() {
     setIsCorrect(isCorrect => temp);
   };
 
-
-
-
-
-
   return (
     <div className="container">
 
@@ -190,7 +190,10 @@ function AccessPage() {
       <Switch>
         <Route exact path="/signUp">
           <div className="right-panel" id="signup" style={{ marginTop: "20px", justifyContent: "flex-start" }}>
-            <form className="userForm" action={"/signup/" + position} method="post" encType="multipart/form-data" >
+            <form className="userForm" action={"/signup/" + position} method="post" encType="multipart/form-data"
+              onSubmit={(e) => {
+                e.target.major.value = majorList.indexOf(e.target.major.value) + 1;
+              }}>
               <div id="nav" >
                 <Nav fill variant="tabs" defaultActiveKey="link-1">
                   <Nav.Item>
@@ -250,9 +253,9 @@ function AccessPage() {
               <div className="input-field" style={{ fontSize: "80%" }}>
                 <i className="fas fa-book-open" style={isCorrect[3] === true ? { color: "var(--color-quarter)" } : null}></i>
                 <label htmlFor="majorList"></label>
-                <input type="text" list="majorList-options" name="major" placeholder="학과를 입력하세요." value={major}
+                <input type="text" list="majorList-options" id='major' name="major" placeholder="학과를 입력하세요."
                   onChange={(e) => {
-                    setMajor(e.target.value);
+                    // setMajor(e.target.value); 없어도될듯?
                     if (majorList.includes(e.target.value)) {
                       changeIsCorrect(3, true);
                     } else {
@@ -262,8 +265,11 @@ function AccessPage() {
                   }></input>
                 <datalist id="majorList-options" >
                   {
-                    majorList.map((major, i) => {
-                      return <option value={major} key={i}></option>
+                    majorList.map((majorName, i) => {
+                      return (
+                        <option value={majorName} key={i} ></option>
+                      )
+
                     })
                   }
                 </datalist>
@@ -359,7 +365,7 @@ function AccessPage() {
               <div className="submitbox" >
                 {signUpButtonState === true
                   ?
-                  <button type="submit" className="SignInBtn">회원가입</button>
+                  <button type="submit" className="SignInBtn" >회원가입</button>
                   :
                   <button type="button" style={{ backgroundColor: '#ACACAC' }} className="SignInBtn" onClick={() => { alert("빈칸을 모두 입력해주세요 :(") }}  >회원가입</button>
                 }
@@ -484,7 +490,7 @@ function AccessPage() {
           </div>
         </Route >
 
-        <Route exact path="/giraffe">
+        <Route exact path="/giraffe-admin">
           <div className="right-panel">
             <form className="userForm">
               <h2 style={{ padding: "20px 0 0 0" }}>관리자 로그인</h2>
@@ -498,11 +504,10 @@ function AccessPage() {
               </div>
 
               <div className="submitbox" >
-                <button type="button" onClick={() => { login() }} value="Login" className="SignInBtn">로그인</button>
+                <button type="button" onClick={() => { setPosition(position => "admin"); login(); }} value="Login" className="SignInBtn">로그인</button>
               </div>
             </form>
           </div>
-
         </Route>
       </Switch >
     </div >
